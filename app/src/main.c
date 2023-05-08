7c451d9b365abf1f36a67f48ddcc358f28c7459f
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2023-05-08 17:36:18 +0200

    app: adjust for single color led (nrf52840dk)
    EOT / Zephyr workshop
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- app/src/main.c
+++ app/src/main.c
@@ -10,6 +10,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/led.h>
 #include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/gpio.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
@@ -24,6 +25,18 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 	BT_UUID_128_ENCODE(0x00001525, 0x1212, 0xefde, 0x1523, 0x785feabcd123)
 #define BT_UUID_LED_ACTION BT_UUID_DECLARE_128(BT_UUID_LED_ACTION_VAL)
 
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/* The devicetree node identifier for the "led1" alias. */
+#define LED1_NODE DT_ALIAS(led1)
+
+/* The devicetree node identifier for the "led2" alias. */
+#define LED2_NODE DT_ALIAS(led2)
+
 /** @brief Bluetooth advertisement data */
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -36,8 +49,9 @@ static const struct bt_data sd[] = {
 	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LED_VAL),
 };
 
-static const struct device *const rgbled =
-		DEVICE_DT_GET(DT_NODELABEL(rgbled));
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 
 /*******************************************************************************
  * Bluetooth services handling
@@ -47,7 +61,7 @@ static ssize_t write_led(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 			 const void *buf, uint16_t len, uint16_t offset,
 			 uint8_t flags)
 {
-	if (len != 3U) {
+	if (len != 1U) {
 		return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
 	}
 
@@ -55,8 +69,8 @@ static ssize_t write_led(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 		return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
 	}
 
-	led_set_color(rgbled, 0U, 3U, (uint8_t *)buf);
-
+	uint8_t val = *((uint8_t *)buf);
+	gpio_pin_set_dt(&led2, val);
 	return len;
 }
 
@@ -73,12 +87,14 @@ static void connected(struct bt_conn *conn, uint8_t err)
 		printk("Connection failed (err %u)\n", err);
 		return;
 	}
+	gpio_pin_set_dt(&led1, true);
 
 	printk("Connected\n");
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
+	gpio_pin_set_dt(&led1, false);
 	printk("Disconnected (reason %u)\n", reason);
 }
 
@@ -87,54 +103,62 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.disconnected = disconnected,
 };
 
-void main(void)
+int main(void)
 {
 #ifdef CONFIG_USB_DEVICE_STACK
 	int ret = usb_enable(NULL);
 	if (ret < 0) {
 		LOG_ERR("Failed to enable USB");
-		return;
+		return 0;
 	}
 #endif
-
-	if (!device_is_ready(rgbled)) {
-		LOG_ERR("RGB LED not ready");
-		return;
+	if (!gpio_is_ready_dt(&led0)) {
+		return 0;
 	}
 
-	/* show some basic colors */
-	printk("RED\n");
-	led_set_color(rgbled, 0U, 3U, (uint8_t []){255U, 0U, 0U});
-	k_msleep(1000);
-	printk("GREEN\n");
-	led_set_color(rgbled, 0U, 3U, (uint8_t []){0U, 255U, 0U});
-	k_msleep(1000);
-	printk("BLUE\n");
-	led_set_color(rgbled, 0U, 3U, (uint8_t []){0U, 0U, 255U});
-	k_msleep(1000);
-	printk("YELLOW\n");
-	led_set_color(rgbled, 0U, 3U, (uint8_t []){255U, 255U, 0U});
-	k_msleep(1000);
-	printk("CYAN\n");
-	led_set_color(rgbled, 0U, 3U, (uint8_t []){0U, 255U, 255U});
-	k_msleep(1000);
-	printk("MAGENTA\n");
-	led_set_color(rgbled, 0U, 3U, (uint8_t []){255U, 0U, 255U});
-	k_msleep(1000);
+	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
 
-	led_off(rgbled, 0U);
+	if (!gpio_is_ready_dt(&led1)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	if (!gpio_is_ready_dt(&led2)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
 
 	/* enable Bluetooth and start advertising */
 	ret = bt_enable(NULL);
 	if (ret < 0) {
 		printk("Bluetooth init failed (ret %d)\n", ret);
-		return;
+		return 0;
 	}
 
 	ret = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd,
 			      ARRAY_SIZE(sd));
 	if (ret < 0) {
 		printk("Advertising failed to start (%d)", ret);
-		return;
+		return 0;
+	}
+	printk("Advertising started (%s)\n", CONFIG_BT_DEVICE_NAME);
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led0);
+		if (ret < 0) {
+			return 0;
+		}
+		k_msleep(SLEEP_TIME_MS);
 	}
 }
